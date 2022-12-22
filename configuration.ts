a8c91318a8ed0016aa75f816305fe38eee7e6fa5
Andrey Arkhipov <arhipov11@yandex.ru>
2022-12-22 21:23:58 +0300

    Added default value for configuration

--- configuration.ts
+++ configuration.ts
@@ -1,13 +1,13 @@
 export default () => ({
   port: parseInt(process.env.APP_PORT, 10) || 3000,
   database: {
-    type: process.env.DB_TYPE,
+    type: process.env.DB_TYPE || 'postgres',
     host: process.env.DB_HOST || 'localhost',
-    port: parseInt(process.env.DB_PORT, 10),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    port: parseInt(process.env.DB_PORT, 10) || 5432,
+    username: process.env.DB_USERNAME || 'student',
+    password: process.env.DB_PASSWORD || 'student',
+    database: process.env.DB_NAME || 'nest_project',
   },
-  secretKey: process.env.SECRET_KEY,
+  secretKey: process.env.SECRET_KEY || '39cf54cebdd3dd01',
   expiresIn: 3600,
 });
