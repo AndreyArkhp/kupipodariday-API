fe5cc13e9ec4f134bafaab6952a75086f058496c
Andrey Arkhipov <arhipov11@yandex.ru>
2022-12-22 09:21:23 +0300

    Fixed guardes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,10 @@
 import { Controller, Post, Body, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { userAboutDefault } from 'src/common/constants';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { ResponseUserDto } from 'src/users/dto/response-user.dto';
 import { UsersService } from 'src/users/users.service';
 import { AuthService } from './auth.service';
-import { LocalGuard } from './guards/local-guard';
 
 @Controller()
 export class AuthController {
@@ -12,7 +12,7 @@ export class AuthController {
     private usersService: UsersService,
     private authService: AuthService,
   ) {}
-  @UseGuards(LocalGuard)
+  @UseGuards(AuthGuard('local'))
   @Post('signin')
   async signin(@Req() user: ResponseUserDto) {
     return this.authService.auth(user);
